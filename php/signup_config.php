e1054b8754737ae87aa1b0e43add1b62007800e8
Charles Martin <cpascual185@gmail.com>
2024-01-02 20:00:33 +0800

    Test 207: Signup

--- php/signup_config.php
+++ php/signup_config.php
@@ -15,10 +15,10 @@
 
         $sql = "INSERT INTO accounts (
             username,
+            email,
             firstname,
             middlename,
             lastname,
-            email,
             birthdate, 
             gender, 
             description, 
@@ -30,16 +30,16 @@
             joindate
         ) VALUES(
             ". $db->validate($_POST["signup-username"]) .", 
+            '". $_POST["signup-email"] ."',
             ". $db->validate($_POST["signup-firstname"]) .",
             ". $db->validate($_POST["signup-middlename"]) .",
             ". $db->validate($_POST["signup-lastname"]) .",
-            ". $_POST["signup-email"] .",
             ". date("Y-m-d", $birthday) .", 
             ". $db->validate($_POST["signup-gender"]) .",
             'N/A',
             1,
             'member',
-            ". $db->validate($_POST["signup-country"]) .",
+            '". $db->validate($_POST["signup-country"]) ."',
             ". $db->validate($_POST["signup-password"]) /* NEEDS HASHING */ .",
             0,
             ". date("Y-m-d H:i:s") . "
