82872431e0c9431648998f4035f196e435bfca89
Charles Martin <cpascual185@gmail.com>
2024-02-23 21:37:40 +0800

    24w07d: Signup Page

--- public/js/src/user/create.js
+++ public/js/src/user/create.js
@@ -13,12 +13,39 @@ document.addEventListener("DOMContentLoaded", (event) => {
     /*for(let i = 1; i <= 12; i++){
         birthmonth.innerHTML += '<'
     }*/
+
+    birthyear.addEventListener("input", intercalarate());
+
+    birthmonth.addEventListener("input", function(){
+        const month = birthmonth.value;
+        let days;
+
+        switch(month){
+            case getMonthWithLeadingZeroStr(Months.February):
+                day = 28;
+                intercalarate();
+                break;
+            case getMonthWithLeadingZeroStr(Months.April):
+            case getMonthWithLeadingZeroStr(Months.June):
+            case getMonthWithLeadingZeroStr(Months.September):
+            case getMonthWithLeadingZeroStr(Months.November):
+                days = 30;
+                break;
+            default:
+                days = 31;
+                break;
+        }
+
+        for(let i = 1; i <= days; i++){
+            birthmonth.innerHTML += '<option value="'+i+'">'+i+'</option>';
+        }
+    });
 });
 
-birthyear.addEventListener("input", function(){
+function intercalarate(){
     const year = birthyear.value;
 
     if(year % 4 == 0){
-        element.innerHTML += '<option value="'+29+'">'+29+'</option>';
+        birthday.innerHTML += '<option value="'+29+'">'+29+'</option>';
     }
-});
+}
