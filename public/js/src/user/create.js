7bd06b5b60a16fcad6cdfd8117d593af9bbe37eb
Charles Martin <cpascual185@gmail.com>
2024-03-01 19:42:03 +0800

    24w08a: Leap years

--- public/js/src/user/create.js
+++ public/js/src/user/create.js
@@ -1,51 +1,70 @@
 //Paradigm: Event-driven
 
 var birthyear = document.getElementById('signup-birthyear');
-var birthmonth = document.getElementById('signup-birthyear');
+var birthmonth = document.getElementById('signup-birthmonth');
 var birthday = document.getElementById('signup-birthday');
+
+var country = document.getElementById('signup-country');
+
+var days = 31;
 
 document.addEventListener("DOMContentLoaded", (event) => {    
     var date = new Date();
-    for(let i = date.getFullYear(); i > 1860; i--){
+    for(let i = date.getFullYear(); i > 1840; i--){
         birthyear.innerHTML += '<option value="'+i+'">'+i+'</option>';
     }
 
-    /*for(let i = 1; i <= 12; i++){
-        birthmonth.innerHTML += '<'
-    }*/
+    birthyear.addEventListener("input", onBirthdateUpdated);
 
-    birthyear.addEventListener("input", intercalarate());
-
-    birthmonth.addEventListener("input", function(){
-        const month = birthmonth.value;
-        let days;
-
-        switch(month){
-            case getMonthWithLeadingZeroStr(Months.February):
-                day = 28;
-                intercalarate();
-                break;
-            case getMonthWithLeadingZeroStr(Months.April):
-            case getMonthWithLeadingZeroStr(Months.June):
-            case getMonthWithLeadingZeroStr(Months.September):
-            case getMonthWithLeadingZeroStr(Months.November):
-                days = 30;
-                break;
-            default:
-                days = 31;
-                break;
-        }
-
-        for(let i = 1; i <= days; i++){
-            birthmonth.innerHTML += '<option value="'+i+'">'+i+'</option>';
-        }
-    });
+    birthmonth.addEventListener("input", onBirthdateUpdated);
+    
 });
+
+function onBirthdateUpdated(){
+    setBirthDays();
+    intercalarate();
+}
+
+function setBirthDays(){
+    const month = birthmonth.value;
+
+    switch(month){
+        case '02':
+            days = 28;
+            break;
+        case '04':
+        case '06':
+        case '09':
+        case '11':
+            days = 30;
+            break;
+        default:
+            days = 31;
+
+            if(birthyear.value == '1844' && birthmonth.value === '12'
+            && country.value === 'Philippines'){
+                days = 30;
+            }
+            break;
+    }
+
+    birthday.innerHTML = '';
+
+    for(let i = 1; i <= days; i++){
+        birthday.innerHTML += '<option value="'+i+'">'+i+'</option>';
+    }
+}
 
 function intercalarate(){
     const year = birthyear.value;
+    const leapDay = document.getElementById("intercalary");
 
-    if(year % 4 == 0){
-        birthday.innerHTML += '<option value="'+29+'">'+29+'</option>';
+    if(!leapDay){
+        if((year % 4 == 0) && (birthmonth.value === '02')){
+            birthday.innerHTML += '<option value="'+29+'" id="intercalary">'+29+'</option>';
+        }else{
+            birthday.innerHTML = '';
+            setBirthDays();
+        }
     }
 }
