e6075cd5b6a157b1feeabc7b194b2bdfb262430e
Charles Martin <cpascual185@gmail.com>
2024-10-23 14:59:26 +0800

    24w42f: Update user index page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,13 +8,20 @@
         <p>{{$message}}</p>
     @endisset
     <h1>{{Auth::user()->username}}</h1>
-    <p>{{Auth::user()->email}}</p>
-    <p>{{Auth::user()->handle}}</p>
-    <p>{{Auth::user()->id}}</p>
-    <p>{{Auth::user()->firstname}} {{Auth::user()->middle_name}}, {{Auth::user()->lastname}}</p>
-    <p>{{Auth::user()->descriptionh}}</p>
-    <p>{{Auth::user()->birthdate}}</p>
-    <p>{{Auth::user()->age}}</p>
+    <p>First Name: {{Auth::user()->firstname}}</p>
+    <p>Middle Name: {{Auth::user()->middlename}}</p>
+    <p>Last Name: {{Auth::user()->lastname}}</p>
+    <p>Email: {{Auth::user()->email}}</p>
+    <p>Handle: {{Auth::user()->handle}}</p>
+    <p>Account ID: {{Auth::user()->account_id}}</p>
+    <p>Description: {{Auth::user()->description}}</p>
+    <p>Birthdate: {{Auth::user()->birthdate}}</p>
+    <p>Age: {{Auth::user()->getAge()}}</p>
+    <p>Gender: {{Auth::user()->gender}}</p>
+    <p>Account Type: {{Auth::user()->type}}</p>
+    <p>Created at: {{Auth::user()->created_at}}</p>
+    <p>Deleted at: {{Auth::user()->deleted_at}}</p>
+    <p>Updated at: {{Auth::user()->updated_at}}</p>
     <br>
     <button onclick="navigate('{{route('user.edit')}}')" class="small-button">Edit</button>
     <button onclick="navigate('{{route('user.logout')}}')" class="small-button">Log out</button>
