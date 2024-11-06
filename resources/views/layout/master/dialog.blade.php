2ac3b8dd391c8e9e7a1e30773f73ddaabdf304fd
Charles Martin <cpascual185@gmail.com>
2024-11-06 12:47:30 +0800

    24w44b: Image upload view files.

---
+++ resources/views/layout/master/dialog.blade.php
@@ -0,0 +1,5 @@
+
+<div class="container dialog">
+    @yield('dialog-title')
+    @yield('dialog-content')
+</div>
---
+++ resources/views/user/account/picture.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.master.dialog')
+@section('dialog-title')
+    <p>Select Profile Picture</p>
+@stop
