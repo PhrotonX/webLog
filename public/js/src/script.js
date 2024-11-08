96cb4b966269a3077c49d5d1be2778e3670d43e2
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-11-08 13:47:43 +0800

    Add toggleDialog() function for Dialog.

--- public/js/src/script.js
+++ public/js/src/script.js
@@ -6,3 +6,11 @@ function navigate(url){
     window.location.href=url;
 }
 
+function toggleDialog(dialogName){
+    var dialogBox = document.getElementById(dialogName);
+    if(dialogBox.style["display"] != "none"){
+        dialogBox.style["display"] = "none";    
+    }else{
+        dialogBox.style["display"] = "block";
+    }
+}
