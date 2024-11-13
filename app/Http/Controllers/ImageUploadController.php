45bef72a78c59bbda9536cfb73f91773887bc63c
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-11-13 12:37:27 +0800

    Display request as debug

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -18,23 +18,26 @@ class ImageUploadController extends Controller
         $data = new Picture();
 
         switch($requestName){
-            case 'edit-profile-picture':
-            case 'upload-profile-picture':
+            case 'edit-profile-picture-form':
+            case 'upload-profile-picture-form':
                 $tempDir  = $this->directory . Auth::id() . "/profile/picture/";
                 break;
-            case 'upload-post-picture':
+            case 'upload-post-picture-form':
                 $tempDir = $this->directory . Auth::id() . "/post/";
                 break;
             default:
                 $tempDir = $this->directory;
         }
-        
 
         if($request->file($requestName)){
             $file = $request->file($requestName);
-            $filename = $date('YmdHIi').file->getClientOriginalName();
+            //$filename = $date('YmdHIi').file->getClientOriginalName();
+            $filename = $tempDir . $file->getClientOriginalName();
             $file->move(public_path($directory), $filename);
             $data['picture_path'] = $filename;
+        }else{
+            //$data['picture_path'] = $this->directory . "image"
+            echo $request;
         }
 
         $data['account_id'] = Auth::user()->account_id;
