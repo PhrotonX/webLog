c3a3fb7cca192e0b47c1d706ce23a94004716f30
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-11-22 12:45:39 +0800

    Improve user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,32 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        for($i = 0; $i < 10; $i++){
-            $gender = '';
-            if(($i % 2) != 0){
-                $gender = 'F';
-            }else{
-                $gender = 'M';
-            }
-
-            $year = date('Y');
-            $birthdate = (($year - 25) + $i) . '1010';
-
-            \App\Models\User::factory()->create([
-                'username' => 'Sample' . $i,
-                'firstname' => 'Sample',
-                'middlename' => '(Testing)',
-                'lastname' => 'Account ' . $i,
-                'handle' => '@sample' . $i,
-                'email' => 'sample'.$i.'@example.com',
-                'password_hash' => bcrypt('sample'.$i),
-                'birthdate' => $birthdate,
-                'gender' => $gender,
-                'country' => 'Philippines',
-                'type' => 'test',
-                'description' => 'Test Account ' . $i . '. For testing purposes only.'
-            ]);
-        }
-        
+        \App\Models\User::factory()->count(10)->create();
     }
 }
