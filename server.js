47f00dad9bedb45fc35b4e48e0f80956816edbd6
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-12-06 20:52:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ const connection = require('connection');
 const app = express();
 app.use(express.json());
 
-app.get('/profile/picture', (req, res) => {
+app.post('/profile/picture', (req, res) => {
     const userId = req.params.user_id;
 
     var query = "SELECT * FROM account_profile_pictures WHERE account_id = " + userId;
     
     connection.query(query, (error, results) => {
-        
+        if(error){
+            console.error('Database querry error: ', error);
+            return res.status(500).json({error: 'Internal Server Error'});
+        }
+        res.json(results);
     });
 });
+
+const PORT = process.env.PORT || 300;
+app.listen(PORT, () => {
+    console.log('Server running on port {$PORT}');
+})
 
