510d62567e6a315ed7a1dc0014504d1f660531f9
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-01-27 13:56:58 +0800

    Fix migration issues.

--- database/migrations/2025_01_27_005540_add_picture_sizes.php
+++ database/migrations/2025_01_27_005540_add_picture_sizes.php
@@ -12,16 +12,23 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('profile_picture', function(Blueprint $table){
-            $table->unsignedBigInteger('pfp_id');
+            $table->bigIncrements('pfp_id');
             $table->unsignedBigInteger('picture_id');
-            $table->string('pfp_small');
-            $table->string('pfp_medium');
-            $table->string('pfp_large');
-            $table->primary(['pfp_id', 'picture_id']);
+            $table->string('pfp_xs')->nullable();
+            $table->string('pfp_small')->nullable();
+            $table->string('pfp_medium')->nullable();
+            $table->string('pfp_large')->nullable();
+            $table->foreign('picture_id')->references('picture_id')->on('picture')->onDelete('cascade');
         });
 
         Schema::table('account_profile_picture', function(Blueprint $table){
             $table->renameColumn('picture_id', 'pfp_id');
+        });
+
+        Schema::table('accounts', function(Blueprint $table){
+            $table->dropForeign(['profile_picture_id']);
+
+            $table->foreign('profile_picture_id')->references('pfp_id')->on('ACCOUNT_PROFILE_PICTURE');
         });
     }
 
@@ -34,6 +41,12 @@ return new class extends Migration
 
         Schema::table('account_profile_picture', function(Blueprint $table){
             $table->renameColumn('pfp_id', 'picture_id');
+        });
+
+        Schema::table('accounts', function(Blueprint $table){
+            $table->dropForeign(['profile_picture_id']);
+
+            $table->foreign('profile_picture_id')->references('picture_id')->on('ACCOUNT_PROFILE_PICTURE');
         });
     }
 };
