071fd5517bd7092c6a109e0c0df467e2a4debd53
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-01-27 13:57:32 +0800

    Update ProfilePictureController.

--- app/Http/Controllers/User/ProfilePictureController.php
+++ app/Http/Controllers/User/ProfilePictureController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\PictureController;
 use App\Models\Picture;
 use App\Models\ProfilePicture;
+use App\Models\AccountProfilePicture;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,26 +23,47 @@ class ProfilePictureController extends PictureController
     }
 
     protected function onSaveToAssociativeTable(Picture $picture){
+        //Obtain the accoutn ID.
         $accountId = Auth::id();
+
+        //Create a ProfilePicture object.
         $profilePicture = new ProfilePicture();
-        
-        $profilePicture['account_id'] = $accountId;
-        $profilePicture['picture_id'] = $picture->id;
+
+        //Set the profile picture foreign ID with the same ID as the picture.
+        $profilePicture['picture_id'] = $picture->picture_id;
+
+        //Make compressed pictures.
+        //onCompressPicture($profilePicture);
 
         $profilePicture->save();
+
+        //Create an associative entity named AccountProfilePicture.
+        $accountProfilePicture = new AccountProfilePicture();
+
+        //Set the data into the associative entity.
+        $accountProfilePicture['account_id'] = $accountId;
+        $accountProfilePicture['pfp_id'] = $profilePicture->pfp_id;
+        
+        
+        $accountProfilePicture->save();
     }
+    
+    /*
+    protected function onCompressPicture(ProfilePicture $picture){
+        //@TODO: Implement this feature.
+    }*/
 
     /**
      * Retrieves all the profile pictures
      * @param id The account id that is associated with the picture.
      */
     public static function getPictures($id) : array{
-        $profilePictures = ProfilePicture::where('account_id', $id)->get();
+        $accountProfilePictures = AccountProfilePicture::where('account_id', $id)->get();
 
         $pictures = [];
 
-        foreach($profilePictures as $profilePicture){
-            $pictures[] = Picture::where('picture_id', $profilePicture->picture_id)->first();
+        foreach($accountProfilePictures as $accountProfilePicture){
+            $pictures[] = ProfilePicture::where('pfp_id', $accountProfilePicture->pfp_id)->first();
         }
 
         return $pictures;
