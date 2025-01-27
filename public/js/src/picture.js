d35162bd35ddfb2a4c629f62fa807311e2225c0e
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-01-27 10:06:24 +0800

    Add picture.ts

---
+++ public/js/src/picture.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Picture = void 0;
+var PictureSize = Object.freeze({
+    Small: "PictureSize.Small",
+    Medium: "PictureSize.Medium",
+    Large: "PictureSize.Large",
+});
+var Picture = /** @class */ (function () {
+    function Picture() {
+    }
+    /**
+     * @param picturePath The path to an image.
+     * @param size The size of an image. Expects a string within PictureSize.
+     * @returns A shrunk image.
+     */
+    Picture.prototype.shrink = function (picturePath, size) {
+    };
+    return Picture;
+}());
+exports.Picture = Picture;
