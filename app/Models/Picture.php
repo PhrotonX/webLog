05d9ea76c3484a163321f3fa342c6986d374c936
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-02-03 13:37:16 +0800

    Add file-related traits into Picture-related entites.

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -1,13 +1,18 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\File;
+use App\Models\FilePicture;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Picture extends Model
 {
+    # Traits
     use HasFactory;
+    use File;
+    use FilePicture;
 
     public $timestamps = false;
 
--- app/Models/ProfilePicture.php
+++ app/Models/ProfilePicture.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use App\Models\Picture;
+use App\Models\File;
+use App\Models\FilePicture;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,6 +14,8 @@ use Illuminate\Database\Eloquent\Model;
 class ProfilePicture extends Model
 {
     use HasFactory;
+    use File;
+    use FilePicture;
 
     public $timestamps = false;
 
@@ -20,6 +24,29 @@ class ProfilePicture extends Model
     protected $primaryKey = 'pfp_id';
     public $incrementing = true;
     protected $keyType = 'int';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    // protected $fillable = [
+    //     'picture_id',
+    //     'pfp_large',
+    //     'pfp_medium',
+    //     'pfp_small',
+    //     'pfp_xs',
+    // ];
+
+    /**
+     * Casts the fields on the table that is NULL by default.
+     */
+    protected $casts = [
+        'pfp_large' => 'string',
+        'pfp_medium' => 'string',
+        'pfp_small' => 'string',
+        'pfp_xs' => 'string',
+    ];
 
     /**
      * Access the Picture model through a foreign key.
